70f59d00faf01cc79c6702cae385553ffb3c63c1
Oliver Hader <oliver@typo3.org>
2013-03-30 10:14:05 +0100

    [TASK] Update ext_emconf.php definitions

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,22 +12,22 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Profile processing for File Abstraction Layer',
-	'description' => 'Profile processing for File Abstraction Layer',
-	'category' => 'plugin',
+	'description' => 'ICC Profile processing for accordant storage of the File Abstraction Layer',
+	'category' => 'misc',
 	'author' => 'Oliver Hader',
 	'author_email' => 'oliver.hader@typo3.org',
 	'author_company' => '',
 	'shy' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => '',
 	'createDirs' => 'uploads/fal_profile',
 	'modify_tables' => 'sys_file_storage',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '0.0.1',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.0',
